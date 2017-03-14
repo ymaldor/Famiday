405aafc4d203f12e357047a15a93c8e585e40caf
Mystiik <guillaume.96@live.fr>
2017-03-14 18:15:13 +0100

    dsfdhjyh

--- src/Template/Layout/left_bar.php
+++ src/Template/Layout/left_bar.php
@@ -3,21 +3,21 @@
 	<div class="sidebar-scroll">
 		<nav class="main-nav">	
 			<ul class="main-menu">
-				<li><a href="Calendar/index"><i class="fa fa-home"></i><span class="text">Accueil</span></a></li>
+				<li><?= $this->Html->link('<i class="fa fa-home"></i><span class="text">Accueil</span>', array('controller' => 'Calendar','action'=> 'index'), array('escape' => false)); ?></li>
 				<li><a href="#" class="js-sub-menu-toggle"><i class="fa fa-navicon"></i><span class="text">Calendrier</span>
 					<i class="toggle-icon fa fa-angle-left"></i></a>
 					<ul class="sub-menu ">
-						<li><a href="nav-default-fixed.html"><span class="text">Bill Jhones</span></a></li>
-						<li><a href="nav-normal.html"><span class="text">Roger le nouveau né</span></a></li>
-						<li><a href="nav-normal.html"><span class="text">Luis Sanchez</span></a></li>
-						<li><a href="nav-normal.html"><span class="text">Wang Fu</span></a></li>
-						<li><a href="nav-normal.html"><span class="text">Herman Gunter</span></a></li>
-						<li><a href="nav-normal.html"><span class="text">Valesca Popozuda</span></a></li>
+						<li><?= $this->Html->link('<span class="text">Bill Jhones</span>', array('controller' => 'Calendar','action'=> 'index'), array('escape' => false)); ?></li>
+						<li><?= $this->Html->link('<span class="text">Roger le nouveau né</span>', array('controller' => 'Calendar','action'=> 'index'), array('escape' => false)); ?></li>
+						<li><?= $this->Html->link('<span class="text">Luis Sanchez</span>', array('controller' => 'Calendar','action'=> 'index'), array('escape' => false)); ?></li>
+						<li><?= $this->Html->link('<span class="text">Wang Fu</span>', array('controller' => 'Calendar','action'=> 'index'), array('escape' => false)); ?></li>
+						<li><?= $this->Html->link('<span class="text">Herman Gunter</span>', array('controller' => 'Calendar','action'=> 'index'), array('escape' => false)); ?></li>
+						<li><?= $this->Html->link('<span class="text">Valesca Popozuda</span>', array('controller' => 'Calendar','action'=> 'index'), array('escape' => false)); ?></li>
 					</ul>
 				</li>
-				<li><a href="Family/gestion"><i class="fa fa-home"></i><span class="text">Gestion famille</span></a></li>
-				<li><a href="Messenger/inbox"><i class="fa fa-home"></i><span class="text">Message</span></a></li>
-				<li><a href="User/deconnexion"><i class="fa fa-home"></i><span class="text">Déconnexion</span></a></li>
+				<li><?= $this->Html->link('<i class="fa fa-home"></i><span class="text">Gestion famille</span>', array('controller' => 'Family','action'=> 'gestion'), array('escape' => false)); ?></li>
+				<li><?= $this->Html->link('<i class="fa fa-home"></i><span class="text">Message</span>', array('controller' => 'Messenger','action'=> 'inbox'), array('escape' => false)); ?></li>
+				<li><?= $this->Html->link('<i class="fa fa-home"></i><span class="text">Déconnexion</span>', array('controller' => 'User','action'=> 'deconnexion'), array('escape' => false)); ?></li>
 			</ul>
 		</nav>
 		<!-- /main-nav -->
